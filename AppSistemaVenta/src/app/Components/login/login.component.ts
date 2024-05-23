9f0cd687c3fdad1edbc818ad6f5c764eb89e6eee
DiegoDoria <retrexretrex@hotmail.com>
2024-05-23 10:37:43 -0500

    Validacion y efectos del login terminado

--- AppSistemaVenta/src/app/Components/login/login.component.ts
+++ AppSistemaVenta/src/app/Components/login/login.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validator, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Login } from 'src/app/Interfaces/login';
+import { UsuarioService } from 'src/app/Services/usuario.service';
+import { UtilidadService } from 'src/app/Reutilizable/utilidad.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +12,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  formularioLogin:FormGroup;
+  ocultarPassword:boolean = true;
+  mostrarLoading:boolean = false;
+  
+  constructor(private fb: FormBuilder, private router:Router, private _usuarioServicio:UsuarioService, private _utilidadServicio: UtilidadService) {
+    this.formularioLogin = fb.group({
+      email:['', Validators.required],
+      password:['', Validators.required]
+    });
+  }
 
   ngOnInit(): void {
+  }
+
+  iniciarSesion(){
+    this.mostrarLoading = true;
+    const request:Login = {
+      correo: this.formularioLogin.value.email,
+      clave: this.formularioLogin.value.password
+    }
+
+    this._usuarioServicio.iniciarSesion(request).subscribe({
+      next:(data) => {
+        if(data.status){
+          this._utilidadServicio.guardarSesionUsuario(data.value)
+          this.router.navigate(['pages'])
+        }
+        else{ this._utilidadServicio.mostrarAlerta('No se encontraron coincidencias', 'Opps!') }
+      },
+      complete: () => { this.mostrarLoading = false; },
+      error: () => { this._utilidadServicio.mostrarAlerta('Hubo un error!', 'Opps!') }
+    })
+
   }
 
 }
